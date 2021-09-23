d3a46e4c5e544345ea527b9693f0255b40947d21
garrettdick <garrettd0511@gmail.com>
2021-09-23 12:53:30 -0500

    commit tree c4aae4d0982394bf0f8f0a45313a73bddeab6573

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
+//The function should receive 1 argument, an Array of JavaScript Objects
+//Each object has two properties: year and result
+//It should use find() to test each Object to see if the result is "W" — a win!
+// It should return the year when the win occurred (if it occurred at all!)
+// If no win is found, it should return, sadly, undefined
 
+
+function superbowlWin(someArray) {
+    let yearWon = someArray.find(resultIsWin); 
+    if (yearWon) {
+        return yearWon.year
+    } else {
+        return yearWon
+    }
+}
+
+function resultIsWin(teamObject) {
+    return teamObject.result === "W"
+}
